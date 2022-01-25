13d5adf23da7ba1fd759b90a540ea2adb7e2da0a
Robert Wilson <32378185+rjw-dev@users.noreply.github.com>
2022-01-25 14:21:42 -0700

    Add files via upload

---
+++ fylo-landing-page-with-two-column-layout-master/js/index.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("jc").value.text;
+
+var z = "red";
+
+//var y = document.GetElementById("myBtn").addEventListener("click", handleClick());
+
+function handleClick(){
+  if( x == z){
+    alert("true");
+  }
+  else{
+    alert("false");
+  }
+}
